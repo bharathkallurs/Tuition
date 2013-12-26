4ec56e66af08b646420ed6d3e34a24453047cf7e
Pavan <pavankowshik@gmail.com>
2013-12-26 11:23:17 +0530

    changes to the sign up popup
    Change-Id: Ib7a2583f3ccd50c02f7af56bcc5bf6fe92be0cb1
    Signed-off-by: Pavan <pavankowshik@gmail.com>

--- WebContent/app/view/Popup/SignUpPopup.view.js
+++ WebContent/app/view/Popup/SignUpPopup.view.js
@@ -10,7 +10,7 @@ sap.ui.jsview("app.view.Popup.SignUpPopup", {
 	 * @returns {array}
 	 */
 	getSideBarItems:function() {
-		return [this._saveButton];
+		return [this._saveButton,this._redoButton];
 	},
 	/**
 	 * Get the current overlay container for this view
@@ -20,6 +20,18 @@ sap.ui.jsview("app.view.Popup.SignUpPopup", {
 		return this._overlayContainer;
 	},
     
+	/**
+	 * Sets the overlay container when this view is used 
+	 * @param overlayContainer {} the overlay container 
+	 * @returns {app.view.Popup.SignUpPopup}
+	 */
+	setOverlayContainer: function(overlayContainer) {
+		this._overlayContainer = overlayContainer;
+		this._overlayContainer.setWidth("600px");
+		this._overlayContainer.setHeight("600px");
+		return this;
+	},
+	
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	* Since the Controller is given to this method, its event handlers can be attached right away. 
 	* @memberOf app.view.Popup.SignUpPopup
@@ -29,11 +41,150 @@ sap.ui.jsview("app.view.Popup.SignUpPopup", {
 		this._viewContentLayout = new sap.ui.commons.layout.VerticalLayout();
 		this._viewContentLayout.setWidth("100%");
 		
+		this._personalInfoPanel= new sap.ui.commons.Panel();
+		this._personalInfoPanel.setTitle(new sap.ui.core.Title({text: "Enter your personal details here"}));
+		
+		this._personalInfoLayout = new sap.ui.commons.layout.VerticalLayout();
+		this._personalInfoLayout.addStyleClass("tutSignUpViewPanels");
+		
+		//create the first name label and field 
+		this._firstNameLabel=  new sap.ui.commons.Label({
+			text:"First Name",
+			required:true
+		});
+		this._firstNameField =  new sap.ui.commons.TextField();
+		this._firstNameField.addStyleClass("tutBottomMargin");
+		this._personalInfoLayout.addContent(this._firstNameLabel);
+		this._personalInfoLayout.addContent(this._firstNameField);
+		
+		//create the last name label and field 
+		this._lastNameLabel=  new sap.ui.commons.Label({
+			text:"Last Name"
+		});
+		this._lastNameField =  new sap.ui.commons.TextField();
+		this._lastNameField.addStyleClass("tutBottomMargin");
+		this._personalInfoLayout.addContent(this._lastNameLabel);
+		this._personalInfoLayout.addContent(this._lastNameField);
+		
+		
+		//create the email label and field 
+		this._emailLabel=  new sap.ui.commons.Label({
+			text:"Email Address",
+			required:true	
+		});
+		this._emailField =  new sap.ui.commons.TextField({
+			width:"250px"
+		});
+		this._emailField.addStyleClass("tutBottomMargin");
+		this._personalInfoLayout.addContent(this._emailLabel);
+		this._personalInfoLayout.addContent(this._emailField);
+		
+		//create the password label and field
+		this._passwordLabel=  new sap.ui.commons.Label({
+			text:"Password",
+			required:true
+		});
+		this._passwordField =  new sap.ui.commons.PasswordField();
+		this._passwordField.addStyleClass("tutBottomMargin");
+		this._personalInfoLayout.addContent(this._passwordLabel);
+		this._personalInfoLayout.addContent(this._passwordField);
+		
+		//create the confirm password label and field
+		this._confpasswordLabel=  new sap.ui.commons.Label({
+			text:"Confirm Password",
+			required:true
+		});
+		this._confpasswordField =  new sap.ui.commons.PasswordField();
+		this._confpasswordField.addStyleClass("tutBottomMargin");
+		this._personalInfoLayout.addContent(this._confpasswordLabel);
+		this._personalInfoLayout.addContent(this._confpasswordField);
+		
+		this._personalInfoPanel.addContent(this._personalInfoLayout);
+		this._viewContentLayout.addContent(this._personalInfoPanel);
+		
+		
+		
+		this._eduInfoPanel= new sap.ui.commons.Panel();
+		this._eduInfoPanel.setTitle(new sap.ui.core.Title({text: "Enter your educational details here"}));
+		
+		this._eduInfoLayout = new sap.ui.commons.layout.VerticalLayout();
+		this._eduInfoLayout.addStyleClass("tutSignUpViewPanels");
+		
+		//create school name label and field 
+		this._shcoolNameLabel=  new sap.ui.commons.Label({
+			text:"School Name",
+			required:true
+		});
+		this._schoolNameField =  new sap.ui.commons.PasswordField();
+		this._schoolNameField.addStyleClass("tutBottomMargin");
+		this._eduInfoLayout.addContent(this._shcoolNameLabel);
+		this._eduInfoLayout.addContent(this._schoolNameField);
+		
+		this._personalInfoPanel.addContent(this._personalInfoLayout);
+		this._viewContentLayout.addContent(this._personalInfoPanel);
+		
+		//create class lable and drop down 
+		this._classNameLabel=  new sap.ui.commons.Label({
+			text:"Class",
+			required:true
+		});
+		this._classDropDown =   new sap.ui.commons.DropdownBox({
+			width:"50px"
+		});
+		this._classDropDown.addStyleClass("tutBottomMargin");
+		this._eduInfoLayout.addContent(this._classNameLabel);
+		this._eduInfoLayout.addContent(this._classDropDown);
+		
+		this._personalInfoPanel.addContent(this._personalInfoLayout);
+		this._viewContentLayout.addContent(this._personalInfoPanel);
+		
+		//create Subjects of Interest label and checkBoxes 
+		this._subsLabel=  new sap.ui.commons.Label({
+			text:"Subjects you are interested in"
+		});
+		this._subsScienceCheckBox = new sap.ui.commons.CheckBox({
+	        text : 'Science',
+	        tooltip : 'Science checkbox',
+	        checked : false
+	    });
+		this._subsMathCheckBox = new sap.ui.commons.CheckBox({
+	        text : 'Maths',
+	        tooltip : 'Maths checkbox',
+	        checked : false
+	    });
+		this._eduInfoLayout.addContent(this._subsLabel);
+		this._eduInfoLayout.addContent(this._subsScienceCheckBox);
+		this._eduInfoLayout.addContent(this._subsMathCheckBox);
+		
+		
+		
+		this._personalInfoPanel.addContent(this._personalInfoLayout);
+		this._viewContentLayout.addContent(this._personalInfoPanel);
+		
+		
+		
+		this._eduInfoPanel.addContent(this._eduInfoLayout);
+		this._viewContentLayout.addContent(this._eduInfoPanel);
+		
+		//create School name label and field 
+		
+		
+		
+		
 		this._saveButton=new sap.ui.commons.Button({
 	        icon : "resources-project/images/save.png",
 	        lite:true,
 	        height:"32px"
 	    });
+		this._saveButton.addStyleClass("tutSaveButton");
+		
+		this._redoButton=new sap.ui.commons.Button({
+	        icon : "resources-project/images/redo.png",
+	        lite:true,
+	        height:"32px"
+	    });
+		this._redoButton.addStyleClass("tutRedoButton");
+		
 		
 	    return this._viewContentLayout;
 	},
