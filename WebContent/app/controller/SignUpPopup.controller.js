27105241f126bdc55099b5fa42a0a2741c8bba55
Pavan <pavankowshik@gmail.com>
2013-12-27 22:13:19 +0530

    Minor code cleanup
    Change-Id: I5680cf5daa26f9bfd8ad3843273d764ea8aea4f3

--- WebContent/app/controller/SignUpPopup.controller.js
+++ WebContent/app/controller/SignUpPopup.controller.js
@@ -3,7 +3,7 @@ sap.ui.controller("app.controller.SignUpPopup", {
     /**
     * Called when a controller is instantiated and its View controls (if available) are already created.
     * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-    * @memberOf app.view.popup.role.RoleDetails
+    * @memberOf app.controller.SignUpPopup
     */
 	onInit: function() {
 		
@@ -28,7 +28,7 @@ sap.ui.controller("app.controller.SignUpPopup", {
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf app.view.popup.role.RoleDetails
+* @memberOf app.controller.SignUpPopup
 */
 //	onBeforeRendering: function() {
 //
@@ -37,7 +37,7 @@ sap.ui.controller("app.controller.SignUpPopup", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf app.view.popup.role.RoleDetails
+* @memberOf app.controller.SignUpPopup
 */
 //	onAfterRendering: function() {
 //
@@ -45,7 +45,7 @@ sap.ui.controller("app.controller.SignUpPopup", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf app.view.popup.role.RoleDetails
+* @memberOf app.controller.SignUpPopup
 */
 //	onExit: function() {
 //
